949659ab20b01170c4563548c7a601c4ab833e29
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-30 18:58:10 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:lib/tests/compile_fail/key_type_not_send.stderr

--- lib/tests/compile_fail/key_type_not_send.rs
+++ lib/tests/compile_fail/key_type_not_send.rs
@@ -2,9 +2,31 @@ use michie::memoized;
 use std::collections::BTreeMap;
 use std::sync::MutexGuard;
 
+struct NotSend(MutexGuard<'static, ()>);
+
+impl PartialEq for NotSend {
+    fn eq(&self, _: &NotSend) -> bool {
+        unreachable!()
+    }
+}
+
+impl Eq for NotSend {}
+
+impl PartialOrd for NotSend {
+    fn partial_cmp(&self, _: &NotSend) -> Option<std::cmp::Ordering> {
+        unreachable!()
+    }
+}
+
+impl Ord for NotSend {
+    fn cmp(&self, _: &Self) -> std::cmp::Ordering {
+        unreachable!()
+    }
+}
+
 fn generic_in_impl() {
-    #[memoized(key_expr = input, store_type = BTreeMap<MutexGuard<'_, ()>, ()>)]
-    fn f(input: MutexGuard<'_, ()>) -> () {
+    #[memoized(key_expr = input, store_type = BTreeMap<NotSend, ()>)]
+    fn f(input: NotSend) -> () {
         ()
     }
 }
